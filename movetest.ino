5f9f3cb0b21b37ac3b356524974806f1a2c12f24
Leyang Zou <leyang.zou@student.tdsb.on.ca>
2023-03-17 18:37:42 -0400

    Update movetest.ino

--- movetest.ino
+++ movetest.ino
@@ -7,22 +7,22 @@
 #include <Wire.h>
 #include "Adafruit_VL6180X.h"
 
-const int pwmLeft = 3;      // left motor speed control
-const int dirLeft = 12;     // left motor direction
-const int pwmRight = 11;    // right motor speed control
-const int dirRight = 13;    // right motor direction
+const int pwmLeft = 3;    // left motor speed control
+const int dirLeft = 12;   // left motor direction
+const int pwmRight = 11;  // right motor speed control
+const int dirRight = 13;  // right motor direction
 
-const int echoLeft = 2;     // left ultrasonic sensor echo
-const int trigLeft = 3;     // left ultrasonic sensor trigger
-const int echoRight = 4;    // right ultrasonic sensor echo
-const int trigRight = 5;    // right ultrasonic sensor trigger
+const int echoLeft = 2;   // left ultrasonic sensor echo
+const int trigLeft = 3;   // left ultrasonic sensor trigger
+const int echoRight = 4;  // right ultrasonic sensor echo
+const int trigRight = 5;  // right ultrasonic sensor trigger
 
-const int ldrIn = A0;       // ldr input
+const int ldrIn = A0;  // ldr input
 
-int startTime;              // tracks the start time of the program
+int startTime;  // tracks the start time of the program
 
-const int SPEED = 160;      // speed of motors (0-255)
-const int SLOW_STEPS = 160;   // number of steps to slow down
+const int SPEED = 160;       // speed of motors (0-255)
+const int SLOW_STEPS = 4;  // number of steps to slow down
 
 // sets up all of the pins and sets the start time
 void setup() {
@@ -41,10 +41,10 @@ void setup() {
 
 void stop() {
   int stepSize = SPEED / SLOW_STEPS;
-  for(int i = 1; i <= SLOW_STEPS; i++) {
-    analogWrite(pwmLeft, SPEED - i * stepSize);
+  for (int i = 1; i <= SLOW_STEPS; i++) {
     analogWrite(pwmRight, SPEED - i * stepSize);
-    delay(1);
+    analogWrite(pwmLeft, SPEED - i * stepSize);
+    delay(10);
   }
   analogWrite(pwmLeft, 0);
   analogWrite(pwmRight, 0);
@@ -53,40 +53,40 @@ void stop() {
 void changeDir(int newLeftDir, int newRightDir) {
   // slow down motors
   int stepSize = SPEED / SLOW_STEPS;
-  for(int i = 1; i <= SLOW_STEPS; i++) {
-    analogWrite(pwmLeft, SPEED - i * stepSize);
+  for (int i = 1; i <= SLOW_STEPS; i++) {
     analogWrite(pwmRight, SPEED - i * stepSize);
-    delay(1);
+    analogWrite(pwmLeft, SPEED - i * stepSize);
+    delay(10);
   }
   // change direction
   digitalWrite(dirLeft, newLeftDir);
   digitalWrite(dirRight, newRightDir);
   // speed up motors
-  for(int i = SLOW_STEPS - 1; i >= 0; i--) {
-    analogWrite(pwmLeft, SPEED - i * stepSize);
+  for (int i = SLOW_STEPS - 1; i >= 0; i--) {
     analogWrite(pwmRight, SPEED - i * stepSize);
-    delay(1);
+    analogWrite(pwmLeft, SPEED - i * stepSize);
+    delay(10);
   }
 }
 
 // makes the sumobot move forward
 void moveForward() {
-  changeDir(LOW, HIGH); // left forward, right forward
+  changeDir(LOW, HIGH);  // left forward, right forward
 }
 
 // makes the sumobot move backward
 void moveBackward() {
-  changeDir(HIGH, LOW); // left backward, right backward
+  changeDir(HIGH, LOW);  // left backward, right backward
 }
 
 // makes the sumobot turn left
 void turnLeft() {
-  changeDir(HIGH, HIGH); // left backward, right forward
+  changeDir(HIGH, HIGH);  // left backward, right forward
 }
 
 // makes the sumobot turn right
 void turnRight() {
-  changeDir(LOW, LOW); // left forward, right backward
+  changeDir(LOW, LOW);  // left forward, right backward
 }
 
 int tmp = 0;
@@ -95,8 +95,8 @@ int tmp = 0;
 void loop() {
   // 5 second initial delay
   int past = millis() - startTime;
-  if(5000 < past && past < 10000) {
-    if(tmp == 0) {
+  if (5000 < past && past < 10000) {
+    if (tmp == 0) {
       digitalWrite(dirLeft, LOW);
       digitalWrite(dirRight, HIGH);
       analogWrite(pwmLeft, SPEED);
@@ -108,8 +108,8 @@ void loop() {
       moveBackward();
       delay(1000);
     }
-  } else if(past > 10000) {
-    if(tmp == 1) {
+  } else if (past > 10000) {
+    if (tmp == 1) {
       stop();
       tmp = 2;
     }
